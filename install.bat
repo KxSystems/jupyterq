5f1a0cacabc3203e511fbf29795b9595001ee7ae
James Hanna <jhanna@kx.com>
2018-03-27 09:37:26 +0100

    error checks

--- install.bat
+++ install.bat
@@ -11,10 +11,10 @@ if not exist %QHOME%\w64\ (
  mkdir %QHOME%\w64 || goto :error
  )
 
-jupyter kernelspec install --user --name=qpk %~dp0\kernelspec || :goto error
-copy /Y %~dp0\jupyterq*.q %QHOME%\ || :goto error
-copy /Y %~dp0\kxpy %QHOME% || :goto error
-copy /Y /B %~dp0\lib\w64\jupyterq.dll %QHOME%\w64\ || :goto error
+jupyter kernelspec install --user --name=qpk %~dp0\kernelspec || goto :error
+copy /Y %~dp0\jupyterq*.q %QHOME%\ || goto :error
+copy /Y %~dp0\kxpy %QHOME% || goto :error
+copy /Y /B %~dp0\lib\w64\jupyterq.dll %QHOME%\w64\ || goto :error
 exit /b 0
 
 :error
