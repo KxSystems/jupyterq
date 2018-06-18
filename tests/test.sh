72e8a80baa8785927095019c2d7a2f9cd208b89b
James Hanna <jhanna@kx.com>
2018-06-18 17:01:47 +0100

    install requirements

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 jupyter nbconvert --allow-errors --to notebook --execute --ExecutePreprocessor.timeout=60 --output tests/test.out kdb+Notebooks.ipynb
-pip -q install jupyter_kernel_test
+pip -q install tests/requirements.txt
 py.test -rx tests/test_jupyterq.py
 
