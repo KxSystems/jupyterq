0a58391383a04f0a52a6ee2e7367959c66ea3f22
James Hanna <jhanna@kx.com>
2018-06-21 12:21:00 +0100

    test html display notebook

--- tests/test.bat
+++ tests/test.bat
@@ -1,5 +1,6 @@
 if defined QLIC_KC (
 	jupyter nbconvert --allow-errors --to notebook --execute --ExecutePreprocessor.timeout=60 --output tests/test.out kdb+Notebooks.ipynb
+	jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --output test.out examples/html_display.ipynb
         pip -q install -r tests\requirements.txt
         pytest -rx tests\test_jupyterq.py
 )
