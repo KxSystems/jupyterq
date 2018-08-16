2f8cd7bc68e5aff2984ed7ef57956e389c983ed0
James Hanna <jhanna@kx.com>
2018-08-16 15:16:55 +0100

    fix escape of latex output, install reqs before tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+pip -q install -r tests/requirements.txt
 jupyter nbconvert --allow-errors --to notebook --execute --ExecutePreprocessor.timeout=60 --output tests/test.out kdb+Notebooks.ipynb
 jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --output test.out examples/html_display.ipynb
-pip -q install -r tests/requirements.txt
 pytest -rx tests/test_jupyterq.py
 
