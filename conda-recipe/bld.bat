1d1ae453dff911966596e8dd36389b88976bb78d
James Hanna <jhanna@kx.com>
2018-08-22 15:47:44 +0100

    kernel.json was not installed by conda package

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -2,7 +2,14 @@ set QHOME=%PREFIX%\q
 mkdir %QHOME%
 where q
 call build\compile.bat
-call install.bat
+copy /Y kernelspec\winkernel.json kernelspec\kernel.json || goto :error
+copy /Y jupyterq*.q? %QHOME%\ || goto :error
+xcopy kxpy %QHOME%\kxpy /I/S/E/H|| goto :error
+copy /Y /B w64\jupyterq.dll %QHOME%\w64\ || goto :error
+set JK=%PREFIX%\share\jupyter\kernels\qpk
+mkdir %JK%
+copy kernelspec\* %JK%
+
 exit /b 0
 :error
 exit /b %errorlevel%
