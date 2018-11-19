f9065e2002a54bbba0d70dabdc1b45624228ae4b
Esperanza <elopezaguilera@kx.com>
2018-11-19 14:29:29 +0000

    Updated tests

---
+++ tests/pswdtest.py
@@ -0,0 +1,33 @@
+import os
+import platform
+
+plsys = platform.system()
+nerr = 0
+def geterr(f,e,l):
+    global nerr
+    if plsys == 'Windows':
+        arg = "tests\kernelspec"+f+" > NUL 2>&1"
+    else:
+        arg = "tests/kernelspec"+f+" > /dev/null 2>&1"
+    os.system("jupyter kernelspec install --user --name=qpk " + arg)
+    if l != "":
+        os.environ['JUPYTERQ_LOGIN'] = l
+    err = os.popen("jupyter-run --kernel=qpk --RunApp.kernel_timeout=3 2>&1").read()
+    if err.find(e)==-1:
+        print("ERROR: "+e)
+        nerr += 1
+	
+folders = ("/ku","/su","/nku","/nsu","","","")
+errors = ("-u not supported, only -U",
+	  "-u not supported, only -U",
+	  "kernel must use -U if server does",
+	  "server must use -U if kernel does",
+	  "Missing JUPYTERQ_LOGIN",
+	  "JUPYTERQ_LOGIN should be user:pass",
+          "Wrong user:password in JUPYTERQ_LOGIN")
+logins = ("","","","","","user-password","user2:password")
+
+for f,e,l in zip(folders,errors,logins):
+    geterr(f,e,l)
+
+print("The number of errors is: {}".format(nerr))
--- tests/test.bat
+++ tests/test.bat
@@ -1,6 +1,14 @@
 if defined QLIC_KC (
         pip -q install -r tests\requirements.txt
-	jupyter nbconvert --allow-errors --to notebook --execute --ExecutePreprocessor.timeout=60 --output tests/test.out kdb+Notebooks.ipynb
-	jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --output test.out examples/html_display.ipynb
+        jupyter nbconvert --allow-errors --to notebook --execute --ExecutePreprocessor.timeout=60 --output tests\test.out kdb+Notebooks.ipynb
+        jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --output test.out examples\html_display.ipynb
+        jupyter nbconvert --allow-errors --to notebook --execute --ExecutePreprocessor.timeout=60 --output remotetest.out examples\remote_example.ipynb
+        pytest -rx tests\test_jupyterq.py         
+        python tests\pswdtest.py
+        jupyter kernelspec install --user --name=qpk tests\kernelspec > NUL 2>&1
+        set JUPYTERQ_LOGIN=user1:password
         pytest -rx tests\test_jupyterq.py
+        set JUPYTERQ_LOGIN=
+        jupyter kernelspec install --user --name=qpk kernelspec > NUL 2>&1
+        
 )
